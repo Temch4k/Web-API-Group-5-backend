3d5be5fa3442cc2ebf57293636f9af441db73b89
Temch4k <artsiom.skarakhod@gmail.com>
2021-04-27 12:38:30 -0600

    Update order.js

--- order.js
+++ order.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
-var Food = require('/Food.js');
+var Food = require('./Food');
 
 mongoose.Promise = global.Promise;
 
